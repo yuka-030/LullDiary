bb5d07b57989dc5ff95d2dcaaa177a7b5322b6f1
yuka-030 <s2.yu.ka.s2.030.vvo0@gmail.com>
2026-08-19 19:48:32 +0900

    feat: アイコンの追加

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,8 +1,23 @@
 // apps/desktop/src/main.tsx
+import { Image } from '@tauri-apps/api/image'
+import { resolveResource } from '@tauri-apps/api/path'
+import { getCurrentWindow } from '@tauri-apps/api/window'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+const appWindow = getCurrentWindow()
+
+// リソースのシステム絶対パスを解決
+const resourcePath = await resolveResource('../public/LullDiary_icon.png')
+
+// パスからImageオブジェクトを生成
+// ※ Cargo.tomlの "image-png" が有効でないとエラーになる
+const icon = await Image.fromPath(resourcePath)
+
+// アイコンを設定
+await appWindow.setIcon(icon)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
